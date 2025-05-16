71253d9ccc816e9b4101a4f606944ffae6f3db24
Fares <faresahmed2004424@gmail.com>
2025-05-16 22:35:24 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/United-Arab-Air-Conditioning",
+  base: "/United-Arab-HVAC-Solutions",
 }));
+ 
